3a96659574d6ad1da1f94e5e1ab5b960285c2dd9
muffinboy <87519281+muffinboy19@users.noreply.github.com>
2023-11-29 21:45:03 +0530

    commit 1
    commit  1

---
+++ adminpanel/database.php
@@ -0,0 +1,6 @@
+<?php
+     $hostname = 'localhost';;               // Hostname
+     $password = ''           	// Password
+     $database = 'gauravdatabase';;    			// Database name
+     $username = 'root';;         		// Datanase Username
+?>
